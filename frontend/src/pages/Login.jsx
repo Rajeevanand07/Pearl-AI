6f35be7436b9fc15881c486028f8ab9d56840df9
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-11-09 01:25:06 +0530

    added event scope

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,6 +23,7 @@ const Login = () => {
     'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events',
   ].join(' '),
 });
 
